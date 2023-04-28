0e57a9ffd5dc16fc45205cd1c0d36c7cea16aafa
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-04-28 11:30:16 +0300

    3 updata

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,16 @@
-const body = document.querySelector('body');
+const bodyEl = document.querySelector('body');
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 let timerId = null;
 
 btnStart.addEventListener('click', () => {
   btnStart.disabled = true;
-  body.style.backgroundColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
   function getRandomHexColor() {
-    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
   timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
